9102190679e1ed99eac654d02189c0c68b9988b9
CRCAE SOFTWARE SOLUTIONS <medicae.mx@gmail.com>
2025-12-23 13:02:11 -0600

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,18 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// --- INICIO DEL FIX para react-native-country-picker-modal ---
+
+// 1. Permitir archivos CJS (CommonJS), necesario para librerías antiguas en web
+config.resolver.sourceExts.push("cjs");
+
+// 2. Forzar a Metro a encontrar 'react-async-hook' correctamente
+// Esto arregla el error: "package itself specifies a main module field that could not be resolved"
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-async-hook': require.resolve('react-async-hook'),
+};
+
+// --- FIN DEL FIX ---
+
 module.exports = withNativeWind(config, { input: "./global.css" });
